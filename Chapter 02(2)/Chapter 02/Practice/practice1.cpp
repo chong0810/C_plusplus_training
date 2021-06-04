1b0b0e5f5f11eb244e19eb73e9d3920b1e9fec01
Daniel Hong <hongchangyeon@gmail.com>
2021-06-03 20:34:37 -0500

    practice 1

---
+++ Chapter 02(2)/Chapter 02/Practice/practice1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name = "Daniel Hong";
+    unsigned int age = 25;
+    unsigned int height = 172;
+    float gpa = 3.2;
+    string middle_initial = "N/A";
+
+    cout << "My name is " << name << " and I am " << age << " years old.\n";
+    cout << "I am " << height << "cm tall.\n";
+    cout << "My overall GPA is " << gpa << ".\n";
+    cout << "My middle initial is " << middle_initial << ".\n";
+
+    return 0;
+}

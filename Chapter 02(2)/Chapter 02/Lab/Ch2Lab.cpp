5ade0620552cfefc333f987446c8b0ebbf1a195b
Daniel Hong <hongchangyeon@gmail.com>
2021-06-06 20:07:15 -0500

    Ch2 Lab

---
+++ Chapter 02(2)/Chapter 02/Lab/Ch2Lab.cpp
@@ -0,0 +1,23 @@
+/*
+Name: Chang Yeon Hong
+Status: Complete
+This program is part of Chapter 2 Lab assignment. It will add two variables and will display them on the console.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1 = 62;
+    int num2 = 99;
+
+    int total = num1 + num2;
+
+    cout << "The sum of num1 and num2 is : " << total << endl;
+    cout << "Chang Yeon Hong" << endl;
+
+
+	return 0;
+}
